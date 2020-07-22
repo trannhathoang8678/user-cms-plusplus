ef7f8cf70c0fb313ae41107b9f8171a683cdd7d3
kiemnx <kiemnguyenxuan@gmail.com>
2020-07-22 21:30:30 +0700

    Add User service

--- src/vn/plusplus/javacore/Application.java
+++ src/vn/plusplus/javacore/Application.java
@@ -1,4 +1,7 @@
 package vn.plusplus.javacore;
 
 public class Application {
+    public static void main(String[] args) {
+
+    }
 }
--- src/vn/plusplus/javacore/services/UserService.java
+++ src/vn/plusplus/javacore/services/UserService.java
@@ -1,6 +1,104 @@
 package vn.plusplus.javacore.services;
 
 import vn.plusplus.javacore.interfaces.UserInterface;
+import vn.plusplus.javacore.models.User;
+
+import java.util.List;
 
 public class UserService implements UserInterface {
+    @Override
+    public List<User> readAllUserFromDB() {
+        return null;
+    }
+
+    @Override
+    public void writeAllUserToDB(List<User> users) {
+
+    }
+
+    @Override
+    public boolean verifyData(User user) {
+        return false;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        List<User> users = readAllUserFromDB();
+        return null;
+    }
+
+    @Override
+    public void saveAccountToDB(User user) {
+        List<User> users = readAllUserFromDB();
+        // Do your code
+
+
+        // End your code
+        writeAllUserToDB(users);
+    }
+
+    @Override
+    public User findUserByUsernameAndPassword(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public boolean verifyEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void sendTokenResetToEmail(String email) {
+
+    }
+
+    @Override
+    public void saveTokenResetToDB(String token, String email, String username) {
+
+    }
+
+    @Override
+    public void displayUsers(List<User> users) {
+
+    }
+
+    @Override
+    public List<User> findUserByRangeAge(Integer fromAge, Integer toAge) {
+        return null;
+    }
+
+    @Override
+    public List<User> findUserByGender(String gender) {
+        return null;
+    }
+
+    @Override
+    public void removeUserToDB(User user) {
+
+    }
+
+    @Override
+    public void verifyNewPass(String newPass, String oldPass) {
+
+    }
+
+    @Override
+    public void updateUserPass(User user, String newPass) {
+
+    }
+
+    @Override
+    public boolean verifyUpdateInfo(String email, String phone, String address) {
+        return false;
+    }
+
+    @Override
+    public void updateUserInfo(User user, String email, String phone, String address) {
+
+    }
 }
